59f53979cd28aa7814edfc8ac3d67c58bfd3c1c5
DBinaghi <39097246+DBinaghi@users.noreply.github.com>
2021-04-13 13:54:56 +0200

    Added options for message title and full page mode

--- MaintenancePlugin.php
+++ MaintenancePlugin.php
@@ -14,7 +14,11 @@ class MaintenancePlugin extends Omeka_Plugin_AbstractPlugin
      * @var array This plugin's options.
      */
     protected $_options = array(
-        'maintenance_message' => '<p>Omeka is currently under maintenance.</p>',
+		'maintenance_title' => 'Site under maintenance',
+        'maintenance_message' => '<p>Sorry for the inconvenience but we’re performing some maintenance at the moment. We’ll be back online shortly!</p>
+								<br>
+								<p>— The Team</p>',
+		'maintenance_fullpagemode' => 0
     );
 
     public function hookInitialize()
@@ -45,7 +49,7 @@ class MaintenancePlugin extends Omeka_Plugin_AbstractPlugin
     public function hookConfigForm($args)
     {
         $flash = Zend_Controller_Action_HelperBroker::getStaticHelper('FlashMessenger');
-        $flash->addMessage(__('Warning: The site is in maintenance mode! Disable the module to make the site on.'), 'error');
+        $flash->addMessage(__('Warning: the site is in maintenance mode! Disable the module to make the site on.'), 'alert');
 
         $view = get_view();
         echo $view->partial('plugins/maintenance-config-form.php');
