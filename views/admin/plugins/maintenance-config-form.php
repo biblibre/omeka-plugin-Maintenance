28c4582026b34ba3a339a952f1f3e3432a506346
DBinaghi <39097246+DBinaghi@users.noreply.github.com>
2021-04-13 13:56:47 +0200

    Added options for message title and full page mode

--- views/admin/plugins/maintenance-config-form.php
+++ views/admin/plugins/maintenance-config-form.php
@@ -1,9 +1,3 @@
-<?php
-/**
- * @var Omeka_View $this
- */
-?>
-
 <?php echo js_tag('vendor/tinymce/tinymce.min'); ?>
 <script type="text/javascript">
 jQuery(document).ready(function () {
@@ -18,9 +12,23 @@ jQuery(document).ready(function () {
 <fieldset id="fieldset-maintenance"><legend><?php echo __('Maintenance'); ?></legend>
     <div class="field">
         <div class="two columns alpha">
+            <?php echo $this->formLabel('maintenance_title', __('Title')); ?>
+        </div>
+        <div class='inputs five columns omega'>
+            <p class="explanation">
+                <?php echo __('The title of the message to display to visitors when the site is in maintenance mode'); ?>
+            </p>
+			<?php echo $this->formText('maintenance_title', get_option('maintenance_title')); ?>
+        </div>
+    </div>
+    <div class="field">
+        <div class="two columns alpha">
             <?php echo $this->formLabel('maintenance_message', __('Message')); ?>
         </div>
         <div class='inputs five columns omega'>
+            <p class="explanation">
+                <?php echo __('The message to display to visitors when the site is in maintenance mode'); ?>
+            </p>
             <?php echo $this->formTextarea(
                 'maintenance_message',
                 get_option('maintenance_message'),
@@ -30,9 +38,17 @@ jQuery(document).ready(function () {
                     'class' => array('textinput', 'html-editor'),
                  )
             ); ?>
-            <p class="explanation">
-                <?php echo __('The message to display to visitors when the site is in maintenance mode'); ?>
-            </p>
         </div>
     </div>
+	<div class="field">
+		<div class="two columns alpha">
+			<?php echo $this->formLabel('maintenance_fullpagemode', __('Full page mode')); ?>
+		</div>
+		<div class="inputs five columns omega">
+			<p class="explanation">
+				<?php echo __('If checked, displays the sign in full page mode.'); ?>
+			</p>
+			<?php echo $this->formCheckbox('maintenance_fullpagemode', get_option('maintenance_fullpagemode'), null, array('1', '0')); ?>
+		</div>
+	</div>
 </fieldset>
