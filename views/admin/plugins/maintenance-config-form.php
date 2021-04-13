f2354ba7a8a84efec873eaea6d056cd08d2df104
DBinaghi <39097246+DBinaghi@users.noreply.github.com>
2021-04-13 17:35:52 +0200

    Added option to display warnings on admin bars

--- views/admin/plugins/maintenance-config-form.php
+++ views/admin/plugins/maintenance-config-form.php
@@ -9,46 +9,58 @@ jQuery(document).ready(function () {
 
 <p><?= flash() ?></p>
 
-<fieldset id="fieldset-maintenance"><legend><?php echo __('Maintenance'); ?></legend>
-    <div class="field">
-        <div class="two columns alpha">
-            <?php echo $this->formLabel('maintenance_title', __('Title')); ?>
-        </div>
-        <div class='inputs five columns omega'>
-            <p class="explanation">
-                <?php echo __('The title of the message to display to visitors when the site is in maintenance mode'); ?>
-            </p>
-            <?php echo $this->formText('maintenance_title', get_option('maintenance_title')); ?>
-        </div>
-    </div>
-    <div class="field">
-        <div class="two columns alpha">
-            <?php echo $this->formLabel('maintenance_message', __('Message')); ?>
-        </div>
-        <div class='inputs five columns omega'>
-            <p class="explanation">
-                <?php echo __('The message to display to visitors when the site is in maintenance mode'); ?>
-            </p>
-            <?php echo $this->formTextarea(
-                'maintenance_message',
-                get_option('maintenance_message'),
-                array(
-                    'rows' => 5,
-                    'cols' => 60,
-                    'class' => array('textinput', 'html-editor'),
-                 )
-            ); ?>
-        </div>
-    </div>
-    <div class="field">
-        <div class="two columns alpha">
-            <?php echo $this->formLabel('maintenance_fullpagemode', __('Full page mode')); ?>
-        </div>
-        <div class="inputs five columns omega">
-            <p class="explanation">
-                <?php echo __('If checked, displays the sign in full page mode.'); ?>
-            </p>
-            <?php echo $this->formCheckbox('maintenance_fullpagemode', get_option('maintenance_fullpagemode'), null, array('1', '0')); ?>
-        </div>
-    </div>
-</fieldset>
+<div class="field">
+	<div class="two columns alpha">
+		<?php echo $this->formLabel('maintenance_title', __('Title')); ?>
+	</div>
+	<div class='inputs five columns omega'>
+		<p class="explanation">
+			<?php echo __('The title of the message to display to visitors when the site is in maintenance mode.'); ?>
+		</p>
+		<?php echo $this->formText('maintenance_title', get_option('maintenance_title')); ?>
+	</div>
+</div>
+
+<div class="field">
+	<div class="two columns alpha">
+		<?php echo $this->formLabel('maintenance_message', __('Message')); ?>
+	</div>
+	<div class='inputs five columns omega'>
+		<p class="explanation">
+			<?php echo __('The message to display to visitors when the site is in maintenance mode.'); ?>
+		</p>
+		<?php echo $this->formTextarea(
+			'maintenance_message',
+			get_option('maintenance_message'),
+			array(
+				'rows' => 5,
+				'cols' => 60,
+				'class' => array('textinput', 'html-editor'),
+			 )
+		); ?>
+	</div>
+</div>
+
+<div class="field">
+	<div class="two columns alpha">
+		<?php echo $this->formLabel('maintenance_mode_fullpage', __('Full page mode')); ?>
+	</div>
+	<div class="inputs five columns omega">
+		<p class="explanation">
+			<?php echo __('If checked, displays the message in full page mode.'); ?>
+		</p>
+		<?php echo $this->formCheckbox('maintenance_mode_fullpage', get_option('maintenance_mode_fullpage'), null, array('1', '0')); ?>
+	</div>
+</div>
+
+<div class="field">
+	<div class="two columns alpha">
+		<?php echo $this->formLabel('maintenance_show_reminder', __('Show reminder')); ?>
+	</div>
+	<div class="inputs five columns omega">
+		<p class="explanation">
+			<?php echo __('If checked, displays a reminder in the admin bars.'); ?>
+		</p>
+		<?php echo $this->formCheckbox('maintenance_show_reminder', get_option('maintenance_show_reminder'), null, array('1', '0')); ?>
+	</div>
+</div>
